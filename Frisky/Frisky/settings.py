da86c0e86cba0c493fc75a3fc38e5734f82d0616
Nworah-Gabriel <gabrielnworah6@gmail.com>
2024-06-29 21:12:13 +0000

    Added allowed host to the settings.py

--- Frisky/Frisky/settings.py
+++ Frisky/Frisky/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5(a&51(dto^0^5-3hbedu!+h4h(xbw^cp(x!g1ru9s^v^4-*wr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://frisky-5st1.onrender.com/", "*.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
