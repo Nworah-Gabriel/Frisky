2ecb383044514e6b28edfbc770f6596b063a08fc
Nworah-Gabriel <gabrielnworah6@gmail.com>
2024-06-29 21:38:17 +0000

    Added allowed host to the settings.py

--- settings.py
+++ settings.py
@@ -127,7 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
 
 
